17e7e1b5fd3696568da26909547df1a369eadd4f
Pablo Moreira <padacomos2@gmail.com>
2024-02-21 16:12:22 -0300

    fix: update request validator

--- src/modules/events/controllers/contracts/update-events-request.contract.ts
+++ src/modules/events/controllers/contracts/update-events-request.contract.ts
@@ -1,8 +1,31 @@
-import { IsOptional, IsString } from '@nestjs/class-validator';
 import { UpdateEventsDto } from '../../dtos/update-events.dto';
-import { IsDateString } from 'class-validator';
+import {
+  IsDateString,
+  IsOptional,
+  IsString,
+  Validate,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
+@ValidatorConstraint({ name: 'customDateRange', async: false })
+export class CustomDateRangeValidator implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const startDate = args.object['startDate'];
+
+    if (value && startDate && value < startDate) {
+      return false;
+    }
+
+    return true;
+  }
+
+  defaultMessage() {
+    return 'End date cannot be earlier than start date';
+  }
+}
 export class UpdateEventsRequest implements UpdateEventsDto {
   @IsString()
   @IsOptional()
@@ -21,7 +44,8 @@ export class UpdateEventsRequest implements UpdateEventsDto {
   @IsDateString()
   @ApiProperty({
     example: '2024-02-21T14:00:00Z',
-    description: 'End date of the event in ISO format',
+    description: 'endDate of the event in ISO format',
   })
+  @Validate(CustomDateRangeValidator)
   endDate: Date;
 }
